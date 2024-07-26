b255978a952dbeaf918e51daaddba02d1be4b9c3
Jere Leman <56977360+JereLem@users.noreply.github.com>
2024-07-26 20:05:58 +0300

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -4,19 +4,17 @@
    Add a "#elif defined" block to this file to include support
    for a particular motor driver.  Then add the appropriate
    #define near the top of the main ROSArduinoBridge.ino file.
-
-   Modiefied by Jere Leman
    
    *************************************************************/
 
 #ifdef USE_BASE
    
-#ifdef MD04_MOTOR_DRIVER
-  /* Include the MD04 library */
-  #include "MD04MotorShield.h"
+#ifdef POLOLU_VNH5019
+  /* Include the Pololu library */
+  #include "DualVNH5019MotorShield.h"
 
   /* Create the motor driver object */
-  MD04MotorShield drive;
+  DualVNH5019MotorShield drive;
   
   /* Wrap the motor driver initialization */
   void initMotorController() {
@@ -84,6 +82,30 @@
     }
   }
   
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
+  }
+
+#elif defined IBT2
+  /* Include the IBT2 library */
+  #include <DualIBT2MotorShield.h>
+
+  /* Create the motor driver object */
+  DualIBT2MotorShield drive;
+  
+  /* Wrap the motor driver initialization */
+  void initMotorController() {
+    drive.init();
+  }
+
+  /* Wrap the drive motor set speed function */
+  void setMotorSpeed(int i, int spd) {
+    if (i == LEFT) drive.setM1Speed(spd);
+    else drive.setM2Speed(spd);
+  }
+
+  // A convenience function for setting both motor speeds
   void setMotorSpeeds(int leftSpeed, int rightSpeed) {
     setMotorSpeed(LEFT, leftSpeed);
     setMotorSpeed(RIGHT, rightSpeed);
