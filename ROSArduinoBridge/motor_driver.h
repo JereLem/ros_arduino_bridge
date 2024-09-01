0ff4bfd4dc0b233f956929fce7a7bea9bb804ca3
Jere Leman <56977360+JereLem@users.noreply.github.com>
2024-09-01 18:34:10 +0300

    Update motor_driver.h

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -1,15 +1,27 @@
 /***************************************************************
    Motor driver function definitions - by Jere Leman
-   *************************************************************/
+***************************************************************/
 
-#ifdef IBT2
-  #define RPWM_RIGHT 11
-  #define LPWM_RIGHT 10
-  
-  #define RPWM_LEFT 3
-  #define LPWM_LEFT 9
+#ifdef A100_MOTOR_DRIVER
+  // 1 - Motor
+  #define RIGHT_MOTOR_BACKWARD 5
+  #define RIGHT_MOTOR_FORWARD  6
+  #define RIGHT_PWM 3
+
+  // 2 - Motor
+  #define LEFT_MOTOR_FORWARD   9
+  #define LEFT_MOTOR_BACKWARD  10
+  #define LEFT_PWM 11
+
+  // Current & Voltage Output
+  #define CURRENT_PIN A0
+  #define VOLTAGE_PIN A1
 #endif
 
+// Function declarations
 void initMotorController();
 void setMotorSpeed(int i, int spd);
 void setMotorSpeeds(int leftSpeed, int rightSpeed);
+float readCurrent();
+float readVoltage();
+void monitorMotorParameters();
